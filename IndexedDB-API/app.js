37b0f281a8c43a700ba0d3e141fde9aed7169cc5
Navdeep Singh <navdeep.er@gmail.com>
2018-04-06 21:38:51 +0530

    IndexedDB API - movies example

---
+++ IndexedDB-API/app.js
@@ -0,0 +1,15 @@
+const MOVIES = [
+  {
+    title : 'Padamavati',
+    releaseYear: '2018',
+    cast : 'Deepika Padukone, Ranbir Singh'
+  },
+  {
+    title: 'Life',
+    releaseYear: '2017',
+    cast: 'Ryan Reynolds, Rebecca Ferguson'
+  }
+]
+// function addMovie(params) {
+  
+// }
