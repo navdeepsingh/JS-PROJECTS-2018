cea5f04663fe088f8e955c1bac243f3f649edcc3
Navdeep Singh <navdeep.er@gmail.com>
2018-06-06 11:58:12 +0530

    Working on Fetch API

---
+++ FetchAPI/NYT/script.js
@@ -0,0 +1,38 @@
+(function() {
+  const APIKEY = '9734b9ee1ec54639ab9bd6c2823b0629';
+  const SEARCH_TERM = 'nature';
+  const ENDPOINT = `http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${APIKEY}&q=${SEARCH_TERM}`;
+
+
+  fetch(ENDPOINT)
+    .then(response => response.json())
+    .then(addArticles)
+    .catch(err => requestError(err, 'articles'));
+
+  function addArticles(data) {
+    const responseContainer = document.querySelector('#response-container');
+    let htmlContent = '';
+
+    if (data.response && data.response.docs && data.response.docs.length > 1) {
+      const articles = data.response.docs;
+      console.log(articles);
+      htmlContent = '<ul>' + articles.map(article => {
+        return `<li>
+                  <h2><a href="${article.web_url}" target="_blank">${article.snippet}</a></h2>
+                  <small>${article.document_type}</small>
+                </li>`;
+      }).join('');
+    } else {
+      htmlContent = '<div class="error-no-articles">No Articles Fetched</div>';
+    }
+    //console.log(responseContainer);
+
+    responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+  }
+
+  function requestError(e, part) {
+    console.log(e);
+    //responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+  }
+
+})();
