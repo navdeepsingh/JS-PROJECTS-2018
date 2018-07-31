4f17907fe990d6787cda252ec6153ed9f0bd92b2
Navdeep Singh <navdeep.er@gmail.com>
2018-07-31 13:34:20 +0530

    Fetch API code updated and Hosted on Firebase

--- FetchAPI/scripts/app.js
+++ FetchAPI/scripts/app.js
@@ -1,31 +1,52 @@
-const ENDPOINT = 'https://reqres.in/api/users';
+const POSTENDPOINT = 'https://reqres.in/api/users';
+const GETENDPOINT = 'https://api.unsplash.com/photos/random?client_id=74afffb9a7b7829d0439d12c87e2897797059b651fd39cd33d3c5ebc1293e4f8';
 
 document.addEventListener('DOMContentLoaded', function() {
-  const FORM = document.querySelector('form');
-  FORM.addEventListener('submit', function(event) {
+  const POSTFORM = document.querySelector('#postForm');
+  POSTFORM.addEventListener('submit', function(event) {
     event.preventDefault();
-    const FORM_DATA = new FormData(FORM);
-    sendRequest(FORM_DATA);
-  })
-})
+    const FORM_DATA = new FormData(POSTFORM);
+    sendPostRequest(FORM_DATA);
+  });
 
-function sendRequest(data) {
+  const GETFORM = document.querySelector('#getForm');
+  GETFORM.addEventListener('submit', function (event) {
+    event.preventDefault();
+    sendGetRequest();
+  });
+
+});
+
+function sendPostRequest(data) {
   let newData = {};
   for( var entry of data.entries() ) {
     newData[entry[0]] = entry[1];
   }
-  
-  fetch(ENDPOINT, {
+
+  fetch(POSTENDPOINT, {
       method: 'POST',
       body: JSON.stringify(newData),
       headers: {
         "Accept": "application/json"
-      }  
+      }
   })
   .then(response => {
-    console.log(response.json());  
+    console.log(response.json());
   })
   .catch(err => {
     throw err;
+  });
+}
+
+function sendGetRequest() {
+  window.URL = window.URL || window.webkitURL;
+  fetch(GETENDPOINT)
+  .then(response => {
+    return response.json();
+  })
+  .then(response => {
+    const imageSrc = response.urls.small;
+    let output = document.querySelector('output');
+    output.innerHTML = `<img src="${imageSrc}" />`;
   });
 }
